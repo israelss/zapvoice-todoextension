d82fa8f7b4e906614a834342fd1326c57d67888d
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-24 16:09:42 -0300

    feat(api): add users module, service and dto

---
+++ api/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: createUserDto,
+    });
+  }
+
+  findOneByEmail(email: string) {
+    console.log({ email });
+    return this.prismaService.user.findUnique({
+      where: { email },
+    });
+  }
+}

9d1fa04146903e868b5c27da51ba2ee6e768177e
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-24 16:12:58 -0300

    feat(api): import modules on AppModule
    - ConfigModule
    - PrismaModule
    - AuthModule
    - UsersModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { ItemsModule } from './items/items.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ItemsModule,
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

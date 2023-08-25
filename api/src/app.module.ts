15ccd59b784d8406c6f90dcf7402b98f7714663d
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-24 21:43:46 -0300

    chore(api): remove unused files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ItemsModule } from './items/items.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -15,7 +13,5 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

65e94895a08849751e3e3c0b96e2ede5061a0e47
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-24 21:46:31 -0300

    feat(api): add completed field as optional to UpdateItemDto

--- api/src/items/dto/update-item.dto.ts
+++ api/src/items/dto/update-item.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateItemDto } from './create-item.dto';
 
-export class UpdateItemDto extends PartialType(CreateItemDto) {}
+export class UpdateItemDto extends PartialType(CreateItemDto) {
+  @IsBoolean()
+  @IsOptional()
+  completed?: boolean;
+}

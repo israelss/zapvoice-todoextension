0f73cf389ec07e97b85443a3b6520af7804573fe
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-27 18:15:59 -0300

    chore(extension): update vite-env.d.ts

--- extension/src/vite-env.d.ts
+++ extension/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_AUTH_ERROR_KEY: string;
+  readonly VITE_EMAIL_KEY: string;
+  readonly VITE_ITEMS_ERROR_KEY: string;
+  readonly VITE_TOKEN_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

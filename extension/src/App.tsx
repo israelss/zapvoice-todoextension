0a994da6dca0616c7adb0c5bd943f5177f1164f9
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-27 17:46:25 -0300

    chore(extension): init repo
    remove unused files that came with boilerplate
    add tailwindcss
    add shadcn components
    add lucide-react icon lib
    add crxjs
    add Dockerfile
    
    update root docker-compose.yaml
    
    add cors settings on api

---
+++ extension/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Extension</h1>;
+}
+
+export default App;
---
+++ extension/vite.config.ts
@@ -0,0 +1,20 @@
+import type { ManifestV3Export } from "@crxjs/vite-plugin";
+import { crx } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import manifest from "./manifest.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest: manifest as ManifestV3Export })],
+  server: {
+    host: true,
+    port: 3001,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});

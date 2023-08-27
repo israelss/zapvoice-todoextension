f935e86f7a33fbca03425e5193596dcc4388ba62
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-27 16:50:15 -0300

    feat(api): update login and register dto

--- api/src/auth/dto/login.dto.ts
+++ api/src/auth/dto/login.dto.ts
@@ -5,12 +5,16 @@ export class LoginDto {
   email: string;
 
   @IsNotEmpty()
-  @IsStrongPassword({
-    minLength: 8,
-    minLowercase: 2,
-    minUppercase: 2,
-    minNumbers: 2,
-    minSymbols: 2,
-  })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'A senha precisa ter no mínimo 8 dígitos com pelo menos 1 número e 1 símbolo',
+    },
+  )
   password: string;
 }

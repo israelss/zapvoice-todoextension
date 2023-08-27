fb6b47ec4236ac81fe863a9be493e2f67ea7d3b7
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-27 18:26:37 -0300

    feat(extension): add ItemPage and AuthPage to App

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,5 +1,19 @@
+import { Toaster } from "@/components/ui/toaster";
+import { useAuth } from "@/hooks/useAuth";
+import { AuthPage } from "@/pages/AuthPage";
+import { ItemsPage } from "@/pages/ItemsPage";
+
 function App() {
-  return <h1>Extension</h1>;
+  const { isAuthorized } = useAuth();
+
+  return (
+    <>
+      <div className="flex flex-col min-h-[422px] w-[400px] p-4">
+        {isAuthorized ? <ItemsPage /> : <AuthPage />}
+      </div>
+      <Toaster />
+    </>
+  );
 }
 
 export default App;

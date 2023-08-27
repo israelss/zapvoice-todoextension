2c67b47fe4d1acae9214b4042b1d0fceb7d618eb
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-27 18:28:59 -0300

    feat(extension): add show password button to input

--- extension/src/components/ui/input.tsx
+++ extension/src/components/ui/input.tsx
@@ -1,25 +1,50 @@
-import * as React from "react"
+"use client";
 
-import { cn } from "@/lib/utils"
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+    const [showPassword, setShowPassword] = React.useState(false);
 
-export { Input }
+    const togglePasswordVisibility = () => {
+      setShowPassword(!showPassword);
+    };
+
+    const inputType = type === "password" && showPassword ? "text" : type;
+    return (
+      <div className="relative">
+        <input
+          type={inputType}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {type === "password" && (
+          <button
+            type="button"
+            className="absolute -translate-y-1/2 right-3 top-1/2"
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? (
+              <EyeIcon className="w-4 h-4 text-gray-500" />
+            ) : (
+              <EyeOffIcon className="w-4 h-4 text-gray-500" />
+            )}
+          </button>
+        )}
+      </div>
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };

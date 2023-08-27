aa145d207b82eb620211c54022774ba9f6bcae3d
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-27 18:23:47 -0300

    feat(extension): create TodoItem

---
+++ extension/src/components/TodoItem.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useItems } from "@/hooks/useItems";
+import { Item } from "@/interfaces";
+import { cn } from "@/lib/utils";
+import { Trash2Icon } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
+
+export const TodoItem = ({ item }: { item: Item }) => {
+  const { markItemAsComplete, removeItem } = useItems();
+  return (
+    <div className="flex items-center justify-between w-[334px] min-h-[47px] gap-2">
+      <div className="flex flex-col flex-1 w-0">
+        <p
+          className={cn(
+            item.completed && "line-through",
+            "font-semibold break-words"
+          )}
+        >
+          {item.content}
+        </p>
+        <div className="text-[0.6rem] grid grid-cols-2">
+          <p>Adicionada:</p>
+          <p>
+            {Intl.DateTimeFormat("pt-br", {
+              dateStyle: "short",
+              timeStyle: "medium",
+            }).format(new Date(item.created_at))}
+          </p>
+        </div>
+        {item.completed && (
+          <div className="text-[0.6rem] grid grid-cols-2">
+            <p>Finalizada:</p>
+            <p>
+              {Intl.DateTimeFormat("pt-br", {
+                dateStyle: "short",
+                timeStyle: "medium",
+              }).format(new Date(item.updated_at))}
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="flex-none w-[85px]">
+        {item.completed ? (
+          <div className="flex items-center gap-1">
+            <Badge className=" bg-cyan-200 text-cyan-600" variant={"outline"}>
+              Feito
+            </Badge>
+            <Button
+              variant="destructive"
+              size="icon"
+              onClick={() => removeItem(item.id)}
+            >
+              <Trash2Icon className="w-4 h-4" />
+            </Button>
+          </div>
+        ) : (
+          <Button
+            disabled={item.completed}
+            onClick={() => markItemAsComplete(item.id)}
+            size={"sm"}
+            variant={item.completed ? "ghost" : "default"}
+            className={"bg-emerald-500 text-emerald-100"}
+          >
+            Completar
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};

9951697ce8b100bf9a943694582a2bc7e0f670a8
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-29 17:19:45 -0300

    fix(extension): clear the errors on every item method

--- extension/src/scripts/items.ts
+++ extension/src/scripts/items.ts
@@ -9,27 +9,30 @@ import {
 import { errorKey, runtime, storage } from "@/lib/utils";
 
 export const getItems = async () => {
+  await storage.remove(errorKey);
   const data = await Api.items.getAll();
   processData(data);
 };
 
 export const createItem = async (payload: CreateItemRequestPayload) => {
+  await storage.remove(errorKey);
   const data = await Api.items.create(payload);
   data.ok ? getItems() : storage.setError(data.message);
 };
 
 export const completeItem = async (payload: ItemIdPayload) => {
+  await storage.remove(errorKey);
   const data = await Api.items.markAsComplete(payload);
   data.ok ? getItems() : storage.setError(data.message);
 };
 
 export const removeItem = async (payload: ItemIdPayload) => {
+  await storage.remove(errorKey);
   const data = await Api.items.remove(payload);
   data.ok ? getItems() : storage.setError(data.message);
 };
 
 async function processData(data: ApiSuccessData<Item[]> | ApiErrorMessage) {
-  await storage.remove(errorKey);
   if (data.ok) {
     runtime.sendMessage({
       type: "GET_ITEMS_RESPONSE",

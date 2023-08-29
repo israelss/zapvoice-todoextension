6d0c6013464bc0872f11997263860a618fa72bf3
Israel Sant'Anna <israel.santanna@gmail.com>
2023-08-29 16:41:04 -0300

    feat(extension): set error changes tracking at App

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,10 +1,47 @@
 import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
 import { useAuth } from "@/hooks/useAuth";
+import { StorageChangesCallback } from "@/interfaces";
+import { errorKey, storage } from "@/lib/utils";
 import { AuthPage } from "@/pages/AuthPage";
 import { ItemsPage } from "@/pages/ItemsPage";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
   const { isAuthorized } = useAuth();
+  const { toast, dismiss } = useToast();
+
+  const [toastId, setToastId] = useState<string | null>(null);
+
+  const showErrorToast = useCallback(
+    (message: string) =>
+      toast({
+        variant: "destructive",
+        title: "Erro",
+        description: message,
+      }),
+    [toast],
+  );
+
+  useEffect(() => {
+    const onStorageChanged: StorageChangesCallback = (changes) => {
+      if (changes[errorKey]?.newValue !== undefined) {
+        const { id } = showErrorToast(changes[errorKey].newValue);
+        setToastId(id);
+      } else {
+        if (toastId !== null) {
+          dismiss(toastId);
+        }
+        storage.remove(errorKey);
+      }
+    };
+
+    storage.onChanged.addListener(onStorageChanged);
+
+    return () => {
+      storage.onChanged.removeListener(onStorageChanged);
+    };
+  }, [dismiss, showErrorToast, toastId]);
 
   return (
     <>
